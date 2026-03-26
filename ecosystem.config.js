48dfd781fa1d7d9be3b520ce94a95d45dcadadfb
AchiniPramo <nimalsampath96@gmail.com>
2026-03-27 00:11:08 +0530

    update ecosystem configuration to optimize Java memory settings for user-service, item-service, and order-service

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  apps : [
-    {
-      name   : "user-service",
-      script : "java -jar user-service/target/User-Service-1.0.0.jar",
-      log_file: "./logs/user-service.log",
-      instances: 2,
-    },
-    {
-      name   : "item-service",
-      script : "java -jar item-service/target/Item-Service-1.0.0.jar",
-      log_file: "./logs/item-service.log",
-      instances: 2,
-    },
-    {
-      name   : "order-service",
-      script : ".java -jar order-service/target/Order-Service-1.0.0.jar",
-      log_file: "./logs/order-service.log",
-      instances: 2,
-    }
-  ]
+    apps : [
+        {
+            name   : "user-service",
+            script : "java",
+            args   : "-Xmx128m -jar user-service/target/User-Service-1.0.0.jar",
+            log_file: "./logs/user-service.log",
+            instances: 2,
+        },
+        {
+            name   : "item-service",
+            script : "java",
+            args   : "-Xmx128m -jar item-service/target/Item-Service-1.0.0.jar",
+            log_file: "./logs/item-service.log",
+            instances: 2,
+        },
+        {
+            name   : "order-service",
+            script : "java",
+            args   : "-Xmx128m -jar order-service/target/Order-Service-1.0.0.jar",
+            log_file: "./logs/order-service.log",
+            instances: 2,
+        }
+    ]
 }
