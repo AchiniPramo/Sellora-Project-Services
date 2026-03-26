e88b7e4a77b889587bb8c6c5b6ac526f3fd49edc
AchiniPramo <nimalsampath96@gmail.com>
2026-03-27 00:55:34 +0530

    update ecosystem configuration to increase instances for user-service, item-service, and order-service; modify order-service database URL

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,21 +4,21 @@ module.exports = {
       name   : "user-service",
       script : "java",
       args   : "-Xmx128m -jar user-service/target/User-Service-1.0.0.jar",
-      instances: 1,
+      instances: 2,
       exec_mode: "fork"
     },
     {
       name   : "item-service",
       script : "java",
       args   : "-Xmx128m -jar item-service/target/Item-Service-1.0.0.jar",
-      instances: 1,
+      instances: 2,
       exec_mode: "fork"
     },
     {
       name   : "order-service",
       script : "java",
       args   : "-Xmx128m -jar order-service/target/Order-Service-1.0.0.jar",
-      instances: 1,
+      instances: 2,
       exec_mode: "fork"
     }
   ]
