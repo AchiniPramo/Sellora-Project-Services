30bfd3c1673d8709f26c69f8c66f92cdad84f5ad
AchiniPramo <nimalsampath96@gmail.com>
2026-03-27 00:22:21 +0530

    refactor ecosystem configuration to simplify service script definitions and add log file paths

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,24 +2,21 @@ module.exports = {
   apps : [
     {
       name   : "user-service",
-      script : "java",
-      args   : "-Xmx128m -jar user-service/target/User-Service-1.0.0.jar",
+      script : "java -jar user-service/target/User-Service-1.0.0.jar",
+      log_file: "./logs/user-service.log",
       instances: 2,
-      exec_mode: "fork"
     },
     {
       name   : "item-service",
-      script : "java",
-      args   : "-Xmx128m -jar item-service/target/Item-Service-1.0.0.jar",
+      script : "java -jar item-service/target/Item-Service-1.0.0.jar",
+      log_file: "./logs/item-service.log",
       instances: 2,
-      exec_mode: "fork"
     },
     {
       name   : "order-service",
-      script : "java",
-      args   : "-Xmx128m -jar order-service/target/Order-Service-1.0.0.jar",
+      script : ".java -jar order-service/target/Order-Service-1.0.0.jar",
+      log_file: "./logs/order-service.log",
       instances: 2,
-      exec_mode: "fork"
     }
   ]
 }
